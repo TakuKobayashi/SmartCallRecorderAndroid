178fd78b2d51cef2f20cbb790b46bf704a6a2cc8
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2025-08-03 00:46:50 +0900

    first commit

---
+++ app/src/main/java/net/taptappun/smartcallrecorder/MainActivity.kt
@@ -0,0 +1,33 @@
+package net.taptappun.smartcallrecorder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import net.taptappun.smartcallrecorder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Example of a call to a native method
+        binding.sampleText.text = stringFromJNI()
+    }
+
+    /**
+     * A native method that is implemented by the 'smartcallrecorderandroid' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'smartcallrecorderandroid' library on application startup.
+        init {
+            System.loadLibrary("smartcallrecorder")
+        }
+    }
+}
